35ad002d665bc578bb3b9013e8513b40a3248511
EducentyOlamide <107129970+EducentyOlamide@users.noreply.github.com>
2024-08-26 14:01:51 +0100

    video conference app 1st

---
+++ src/app/page.tsx
@@ -0,0 +1,76 @@
+
+"use client";
+
+import {
+  ControlBar,
+  GridLayout,
+  LiveKitRoom,
+  ParticipantTile,
+  RoomAudioRenderer,
+  useTracks,
+  PreJoin
+} from "@livekit/components-react";
+import "@livekit/components-styles";
+import { Track } from "livekit-client";
+import { useEffect, useState } from "react";
+
+export default function Page() {
+  const [token, setToken] = useState("");
+  const [userChoices, setUserChoices] = useState(null);
+  const iD = Math.floor(Math.random() * 100)
+
+
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const resp = await fetch(
+          `/api/get-participant-token?room=room&username=${iD}`
+        );
+        const data = await resp.json();
+        setToken(data.token);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [iD]);
+
+  if (token === "") {
+    return <div>Getting token...</div>;
+  }
+
+  
+
+  return (
+    
+    <LiveKitRoom
+      video={true}
+      audio={true}
+      token={token}
+      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+      data-lk-theme="default"
+      style={{ height: '100dvh' }}
+    >
+      <MyVideoConference />
+      <RoomAudioRenderer />
+      <ControlBar />
+    </LiveKitRoom>
+    
+  );
+}
+
+function MyVideoConference() {
+
+  const tracks = useTracks(
+    [
+      { source: Track.Source.Camera, withPlaceholder: true },
+      { source: Track.Source.ScreenShare, withPlaceholder: false },
+    ],
+    { onlySubscribed: false },
+  );
+  return (
+    <GridLayout tracks={tracks} style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
+      <ParticipantTile />
+    </GridLayout>
+  );
+}
